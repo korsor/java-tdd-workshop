b2723439d14aebfc36a6d30e7a7813b238d3562a
Tapakorn Wankaew <tapakorn.w@kasikornbank.com>
2017-07-20 10:53:10 +0700

    case12

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,5 +1,7 @@
 public class FizzBuzz {
     public String say(int input) {
+        if (input % 15 == 0)
+            return "FizzBuzz";
         if (input % 3 == 0)
             return "Fizz";
         if (input % 5 == 0)
